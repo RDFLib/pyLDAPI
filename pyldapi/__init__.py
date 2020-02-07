65a3862329dca175bcf04dafbfd248196f58c246
Nicholas Car <nicholas.car@csiro.au>
2020-02-07 21:21:49 +1000

    v3.4 bump

--- pyldapi/__init__.py
+++ pyldapi/__init__.py
@@ -6,7 +6,7 @@ from pyldapi.renderer_container import ContainerRenderer, ContainerOfContainersR
 from pyldapi.profile import Profile
 from pyldapi.helpers import setup
 
-__version__ = '3.3'
+__version__ = '3.4'
 
 __all__ = [
     'Renderer',
